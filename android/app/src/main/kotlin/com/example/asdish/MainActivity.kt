b06e2351a84b5770cadb20335012a6f162031398
bricekams <kamhouabrice0@gmail.com>
2024-05-15 00:03:34 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/asdish/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.asdish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(const AsDish());
+}
+
+class AsDish extends StatelessWidget {
+  const AsDish({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
